acc1fd335b658e71710017438a84f362f5994767
codesshaman <nicksemail@yandex.ru>
2022-10-07 11:49:39 -0400

    root password

--- project/srcs/requirements/mariadb/conf/create_db.sh
+++ project/srcs/requirements/mariadb/conf/create_db.sh
@@ -22,7 +22,7 @@ DELETE FROM     mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
 CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
 GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
